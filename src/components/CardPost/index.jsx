225f9d4720ffa971aeac22c398d0103ee8f86cbf
Pedro Barbosa <trainee@arcasoltec.com.br>
2025-09-04 14:07:05 -0300

    sixth

--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -5,29 +5,28 @@ import Link from "next/link"
 
 export const CardPost = ({ post }) => {
     return (
-        <Link href={`/posts/${post.slug}`} className={styles.link}>
-            <article className={styles.card}>
-                <header className={styles.header}>
-                    <figure>
-                        <Image
-                            src={post.cover}
-                            width={438}
-                            height={133}
-                            alt={`Capa do post de titulo: ${post.title}`}
-                        />
-                    </figure>
-                </header>
-                <section className={styles.body}>
-                    <h2>{post.title}</h2>
-                    <p>{post.body}</p>
-                </section>
-                <footer className={styles.footer}>
-                    <Avatar
-                        imageSrc={post.author.avatar}
-                        name={post.author.username}
+        <article className={styles.card}>
+            <header className={styles.header}>
+                <figure>
+                    <Image
+                        src={post.cover}
+                        width={438}
+                        height={133}
+                        alt={`Capa do post de titulo: ${post.title}`}
                     />
-                </footer>
-            </article>
-        </Link>
+                </figure>
+            </header>
+            <section className={styles.body}>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+                <Link href={`/posts/${post.slug}`}>Ver detalhes</Link>
+            </section>
+            <footer className={styles.footer}>
+                <Avatar
+                    imageSrc={post.author.avatar}
+                    name={post.author.username}
+                />
+            </footer>
+        </article>
     )
 }
